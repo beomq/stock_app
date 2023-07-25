06a2ec130ad73b6ef83225d28b59df6ff45100fb
beomq <beomdolq@gmail.com>
2023-07-25 23:16:33 +0900

    feat: provider 적용

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:provider/provider.dart';
+import 'package:stock_app/data/repository/stock_repository_impl.dart';
+import 'package:stock_app/data/source/local/stock_dao.dart';
+import 'package:stock_app/data/source/remote/stock_api.dart';
+import 'package:stock_app/presentation/company_listings/company_listings_screen.dart';
+import 'package:stock_app/presentation/company_listings/company_listings_view_model.dart';
 import 'package:stock_app/util/color_schemes.dart';
 import 'package:stock_app/data/source/local/company_listing_entity.dart';
-import 'package:stock_app/presentation/company_info/main_screen.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
 Future<void> main() async {
   await Hive.initFlutter();
   Hive.registerAdapter(CompanyListingEntityAdapter());
   await dotenv.load();
-  runApp(const MyApp());
+
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+            create: (_) => CompanyListingsViewModel(
+                StockRepositoryImpl(StockApi(), StockDao())))
+      ],
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -28,7 +43,7 @@ class MyApp extends StatelessWidget {
         colorScheme: darkColorScheme,
       ),
       themeMode: ThemeMode.system,
-      home: const MainScreen(),
+      home: const CompanyListingsScreen(),
     );
   }
 }
