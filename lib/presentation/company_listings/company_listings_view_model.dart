2e0dc0bbbb8e46c166acbfd9738d7c39f82921dc
beomq <beomdolq@gmail.com>
2023-07-25 23:16:10 +0900

    feat: debounce 적용

--- lib/presentation/company_listings/company_listings_view_model.dart
+++ lib/presentation/company_listings/company_listings_view_model.dart
@@ -1,16 +1,36 @@
+import 'dart:async';
+
 import 'package:flutter/cupertino.dart';
 import 'package:stock_app/domain/repository/stock_repository.dart';
+import 'package:stock_app/presentation/company_listings/company_listings_action.dart';
 import 'package:stock_app/presentation/company_listings/company_listings_state.dart';
 
 class CompanyListingsViewModel with ChangeNotifier {
   final StockRepository _repository;
 
-  var _state = CompanyListingsState();
+  var _state = const CompanyListingsState();
+
+  Timer? _debounce;
 
   CompanyListingsState get state => _state;
 
   CompanyListingsViewModel(this._repository) {
     _getCompanyListings();
+  }
+
+  void onAction(CompanyListingsAction action) {
+    action.when(
+      refresh: () => _getCompanyListings(fetchFromRemote: true),
+      onSearchQueryChange: (query) {
+        _debounce?.cancel();
+        _debounce = Timer(
+          const Duration(milliseconds: 300),
+          () {
+            _getCompanyListings(query: query);
+          },
+        );
+      },
+    );
   }
 
   Future _getCompanyListings({
