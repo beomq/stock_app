3dab3e299e6d38be272f9b33e3f9edb880ecb11c
beomq <beomdolq@gmail.com>
2023-07-25 20:32:19 +0900

    feat: client 부분 수정, 주소 오타 수정

--- lib/data/source/remote/stock_api.dart
+++ lib/data/source/remote/stock_api.dart
@@ -5,13 +5,13 @@ class StockApi {
   final String baseUrl = 'https://www.alphavantage.co';
   final String? apiKey = dotenv.env['ALPHA_VANTAGE_API_KEY'];
 
-  final http.Client client;
+  final http.Client _client;
 
-  StockApi(this.client);
+  StockApi({http.Client? client}) : _client = (client ?? http.Client());
 
   Future<http.Response> getListings({String? apiKey}) async {
     String usedApiKey = apiKey ?? this.apiKey ?? '';
-    return await client.get(
-        Uri.parse('$baseUrl/query?function=LISTING_STATUS&apiKEY=$usedApiKey'));
+    return await _client.get(
+        Uri.parse('$baseUrl/query?function=LISTING_STATUS&apikey=$usedApiKey'));
   }
 }
