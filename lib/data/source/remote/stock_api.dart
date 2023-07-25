cec2421b731366e830518576d65fd048d3eab1d1
beomq <beomdolq@gmail.com>
2023-07-25 19:22:42 +0900

    feat: repository impl 작성에 따른 api 수정

--- lib/data/source/remote/stock_api.dart
+++ lib/data/source/remote/stock_api.dart
@@ -2,15 +2,16 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
 
 class StockApi {
-  final baseUrl = 'https://www.alphavantage.co';
-  final apiKey = dotenv.env['ALPHA_VANTAGE_API_KEY'];
+  final String baseUrl = 'https://www.alphavantage.co';
+  final String? apiKey = dotenv.env['ALPHA_VANTAGE_API_KEY'];
 
   final http.Client client;
 
   StockApi(this.client);
 
-  Future<http.Response> getListings(String apiKey) async {
+  Future<http.Response> getListings({String? apiKey}) async {
+    String usedApiKey = apiKey ?? this.apiKey ?? '';
     return await client.get(
-        Uri.parse('$baseUrl/query?function=LISTING_STATUS&apiKEY=$apiKey'));
+        Uri.parse('$baseUrl/query?function=LISTING_STATUS&apiKEY=$usedApiKey'));
   }
 }
