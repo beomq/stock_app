888e998c68751273c09f0b2c16cef5b43f5ef2b6
beomq <beomdolq@gmail.com>
2023-07-25 18:54:55 +0900

    feat: color schemes를 util 폴더로 이동

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:hive/hive.dart';
-import 'package:stock_app/color_schemes.dart';
+import 'package:stock_app/util/color_schemes.dart';
 import 'package:stock_app/data/source/local/company_listing_entity.dart';
 import 'package:stock_app/presentation/main_screen.dart';
 
