95259c7ca553e63228cc39e94e907b71d85da6bb
beomq <beomdolq@gmail.com>
2023-07-26 00:45:54 +0900

    feat: company info api 작성

--- lib/data/source/remote/stock_api.dart
+++ lib/data/source/remote/stock_api.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
+import 'package:stock_app/data/source/remote/dto/company_info_dto.dart';
 
 class StockApi {
   final String baseUrl = 'https://www.alphavantage.co';
@@ -13,5 +16,16 @@ class StockApi {
     String usedApiKey = apiKey ?? this.apiKey ?? '';
     return await _client.get(
         Uri.parse('$baseUrl/query?function=LISTING_STATUS&apikey=$usedApiKey'));
+  }
+
+  Future<CompanyInfoDto> getCompanyInfo({
+    required String symbol,
+    String? apiKey,
+  }) async {
+    String usedApiKey = apiKey ?? this.apiKey ?? '';
+    final response = await _client.get(Uri.parse(
+        '$baseUrl/query?function=OVERVIEW&symbol=$symbol&apikey=$usedApiKey'));
+
+    return CompanyInfoDto.fromJson(jsonDecode(response.body));
   }
 }
